5546b3d11bdb1b44a07e27883c8d802a772149d6
Tag <teddy_bear_morbid@hotmail.com>
2025-03-24 21:20:11 +1100

    openai api key added

---
+++ caelum-office/build/precache-manifest.81b0d22668c53bbe2a812b96f9ec66c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599255e3a36882b2597054e921b8b5c6",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "8eb7def0885b5f93865f",
+    "url": "/myapp/static/js/2.3d6f98bf.chunk.js"
+  },
+  {
+    "revision": "58833bb1c7181fbf84d8",
+    "url": "/myapp/static/js/main.f83044c5.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
